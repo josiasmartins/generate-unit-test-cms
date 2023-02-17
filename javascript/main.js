2f83faa8ff0a41b1aeb53bb7535a61bbcae9bb18
Caitano <josiasmartinscaitano@gmail.com>
2023-02-17 10:48:56 -0300

    refactor: refactored all method and creat news

--- javascript/main.js
+++ javascript/main.js
@@ -3,52 +3,113 @@ const generate_code__textarea = document.querySelector('.generate-code__textarea
 const insert__textarea = document.querySelector('.insert__textarea');
 const generator_button = document.querySelector('.generator-button');
 
-function extractObject(cms) {
+/**
+ * Funcao princiapl que executa todas as complementares
+ * 
+ * @param cms objeto inteiro do cms 
+ */
+function main(cms) {
     console.log(cms, "cms");
     const nome_component = cms.step.components.type;
     const comp = cms.step.components.component[nome_component];
 
-    for (let name in comp) {
-        let objeto = {
-            nome: name,
-            valor: comp[name]
-        };
+    const firstField =  getFirstField(comp)
 
-        cms_response.push(objeto);
-
-        console.log(cms_response);
-    };
+    percorrerObjetos(comp, cms_response, firstField)
 
     console.log(cms_response, "cms response");
     generateCode(nome_component, comp);
 }
 
+/**
+ * 
+ * @param comp objeto do componente cms 
+ * @returns retorna somente o primeiro campo
+ */
+function getFirstField(comp) {
+    for (let field in comp) {
+        return field
+    }
+}
+
+/**
+ * Função que modifca o indece (keys) do array... remove o ponto e adicione o [] com numero
+ */
+function formaterIndex() {
+    const pattern = /.(\d)./;
+    for (let index of cms_response) {
+        index.keys = index.keys.replace(pattern, '[$1]')
+    }
+}
+
+/**
+ * 
+ * @param objeto nome do componente
+ * @param novoObjeto array que sera adicionado o objeto iterado
+ * @param  keys indece 
+ * @param prefixo complemento do indece
+ */
+function percorrerObjetos(objeto, novoObjeto, keys, prefixo = '') {
+    for (let chave in objeto) {
+      if (typeof objeto[chave] === 'object' && objeto[chave] !== null) {
+        percorrerObjetos(objeto[chave], novoObjeto, keys, prefixo + chave + '.');
+      } else {
+        novoObjeto.push({ nome: chave, valor: objeto[chave], keys: prefixo + chave, deepKeys: prefixo + keys });
+      }
+    }
+
+    formaterIndex()
+}
+
+
+/**
+ *  Transforma o valor do parametro em JSON
+ * 
+ * @param value 
+ * @returns retorna o objeto parseado
+ */
 function transformObjectJs(value) {
     return JSON.parse(value);
 }
 
+/**
+ * Imprime na tag o valor do objeto iterado, concatenando
+ * 
+ * @param  nameComponent nome do componente cms (type)
+ * @param  component_object objeto do componente cms
+ */
 function generateCode(nameComponent, component_object) {
 
     addConst(nameComponent, component_object);
-    
+
     for (let object of cms_response) {
         generate_code__textarea.value += `
             pm.test("Contains ${object.nome} with correctly text", function () {
             pm.expect(pm.response.text()).to.include("${object.nome}");
-            pm.expect(fields.${object.nome}).to.eql(${verifyType(object.valor)});
+            pm.expect(fields.${object.keys}).to.eql(${verifyType(object.valor)});
         });
         `;
-
-        //generate_code__textarea.value += '\n';
     }
 }
 
+/**
+ * Verifca se objeto é igual string... se for adicione o "" no objeto, se não retorne object
+ * 
+ * @param object objeto
+ * @returns objet || "object"
+ */
 function verifyType(object) {
     if (typeof object == 'string') return `"${object}"`;
 
     return object;
 }
 
+/**
+ * Imprime o valor na tag
+ * 
+ * @param nameComponent nome do componente cms (type)
+ * @param component_object objeto do componente cms
+ */
 function addConst(nameComponent, component_object) {
     const lenght = Object.keys(component_object).length;
     generate_code__textarea.value = `
@@ -65,123 +126,12 @@ function addConst(nameComponent, component_object) {
     `;
 }
 
+/**
+ * Função que copia o código gerado
+ */
 function copy() {
     generate_code__textarea.select();
     document.execCommand("copy");
 }
 
-generator_button.addEventListener('click', () => extractObject(transformObjectJs(insert__textarea.value)));
-
-
-
-// import { download } from "./mocks.js";
-
-// const gerador = (cms) => {
-
-//     const generate = function(mock) {
-//         mock = download;
-    
-//         mock.components.component
-//         console.log(mock);
-    
-//         document.querySelector('.mock')
-    
-//     }
-    
-//     function extractObject(cms) {
-
-//         let objeto = [];
-    
-//         const nome_component = cms.step.components.type;
-//         const comp = cms.step.components.component[nome_component];
-//         for (let name in comp) {
-//             let objeto = {
-//                 nome: name,
-//                 valor: comp[name]
-//             };
-//             this.objeto.push(objeto);
-    
-//             if (typeof objeto.valor == 'object') {
-//                 for (let nome2 in objeto.valor) {
-//                     let objeto2 = {
-//                         nome2: nome2,
-//                         valor2: objeto.valor[nome2]
-//                     };
-//                     this.objeto.push(objeto2);
-//                     console.log(objeto2, "objeto 2");
-    
-//                    if (typeof objeto2.valor2 == 'object') {
-//                     for (let nome3 in objeto2.valor2) {
-//                         let objeto3 = {
-//                             nome3: nome3,
-//                             valor3: objeto2.valor[nome3]
-//                         };
-//                         this.objeto.push(objeto3);
-//                         console.log(objeto3, "objeto 3");
-//                     }
-//                    }
-//                 }
-//             }
-//             console.log(objeto)
-//         }
-
-//         return objeto;
-//     }
-
-//     function extractObject2(cms) {
-    
-//         const nome_component = cms.step.components.type;
-//         const comp = cms.step.components.component[nome_component];
-//         for (let name in comp) {
-//             let objeto = {
-//                 nome: name,
-//                 valor: comp[name]
-//             };
-    
-//             if (typeof objeto.valor == 'object') {
-//                 for (let nome2 in objeto.valor) {
-//                     let objeto2 = {
-//                         nome2: nome2,
-//                         valor2: objeto.valor[nome2]
-//                     };
-//                     console.log(objeto2, "objeto 2");
-//                 }
-//             }
-//             console.log(objeto)
-//         }
-//     }
-
-//     function extractObject3(cms) {
-    
-//         const nome_component = cms.step.components.type;
-//         const comp = cms.step.components.component[nome_component];
-//         for (let name in comp) {
-//             let objeto = {
-//                 nome: name,
-//                 valor: comp[name]
-//             };
-
-//             this.objeto.push(objeto)
-    
-//             console.log(objeto)
-//         }
-//     }
-
-//     function inputCms() {
-//         document.querySelector('#cms').va
-//     }
-
-//     function replaceUrl() {
-//         document.querySelector('.toggle').value == "hk";
-
-//     }
-
-//     return {
-//         extractObject
-//     }
-
-// }
-
-
-
-// extractObject(download);
+generator_button.addEventListener('click', () => main(transformObjectJs(insert__textarea.value)));
